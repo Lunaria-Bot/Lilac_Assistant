2c38bebd763916fd40f5a435b20c217306e8e3e7
StarStream <alexiswiesen@yahoo.com>
2025-11-11 13:37:01 +0100

    Update auction_manager.py

--- cogs/auction_manager.py
+++ cogs/auction_manager.py
@@ -108,6 +108,12 @@ class AuctionManager(commands.Cog):
         lowered = message.content.lower()
         if any(word in lowered for word in ACCEPT_KEYWORDS):
             if not message.channel.locked:
+                forum = message.guild.get_channel(message.channel.parent_id)
+                active_tag = discord.utils.find(lambda t: t.id == ACTIVE_TAG_ID, forum.available_tags)
+                if active_tag and active_tag in message.channel.applied_tags:
+                    new_tags = [t for t in message.channel.applied_tags if t.id != ACTIVE_TAG_ID]
+                    await message.channel.edit(applied_tags=new_tags)
+
                 await message.channel.send(
                     "✅ This auction has been accepted please proceed with the trade <:vei_drink:1298164325302931456>"
                 )
